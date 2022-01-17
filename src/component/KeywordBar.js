3c96cc466acd5afbed5938120905d2fc78dff1fe
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-01-17 07:30:23 +0530

    search keywordBar is added

---
+++ src/component/KeywordBar.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+let keyword=[
+    'All',
+    'JavaScript',
+    'React',
+    'Redux',
+    "hooks",
+    'Bootstrap',
+    'Material-Ui',
+    'Movies',
+    'songs',
+    'All',
+    'JavaScript',
+    'React',
+    'Redux',
+    "hooks",
+    'Bootstrap',
+    'Material-Ui',
+    'Movies',
+    'songs',
+    'All',
+    'JavaScript',
+    'React',
+    'Redux',
+    "hooks",
+    'Bootstrap',
+    'Material-Ui',
+    'Movies',
+    'songs',
+    'All',
+    'JavaScript',
+    'React',
+    'Redux',
+    "hooks",
+    'Bootstrap',
+    'Material-Ui',
+    'Movies',
+    'songs',
+]
+const Container = styled.div`
+background-color: #212121;
+border: 1px solid #3d3d3d;
+color: white;
+display: flex;
+overflow-x: scroll;
+padding-top: 15px;
+font-size: 18px;
+&::-webkit-scrollbar{
+   width: 0px;
+}
+`
+const Span = styled.span`
+height: 100%;
+padding: 5px 10px;
+border: 1px solid black;
+border-radius: 999px;
+margin-right:10px;
+white-space: nowrap;
+background-color: ${props=> props.type? 'red':'black'};
+`
+
+
+export default function KeywordBar() {
+    const [activeElement, setactiveElement] = useState("All")
+
+    const handleClick = (value)=>{
+        setactiveElement(value)
+    }
+    console.log('activeElement :',activeElement)
+    return (
+        <Container>
+            
+                {keyword.map((e,i)=> <Span type={activeElement===e? 'active':''}  onClick={()=>handleClick(e)} key={i}> {e}</Span>)}
+            
+        </Container>
+    )
+}
