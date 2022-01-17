2f0a021b96961530858cc043205fcbca292ae411
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-01-17 13:07:24 +0530

    data.js file is added to work on video playlist layout

---
+++ src/data.js
@@ -0,0 +1,359 @@
+export const data =  {
+    "kind": "youtube#searchListResponse",
+    "etag": "-kVu-gNRAe0BpWocrxV0JOGRjF4",
+    "nextPageToken": "CAUQAA",
+    "regionCode": "IN",
+    "pageInfo": {
+        "totalResults": 29806,
+        "resultsPerPage": 5
+    },
+    "items": [
+        {
+            "kind": "youtube#searchResult",
+            "etag": "gv_nvCUsptzyA0BwrEysPV5Hj2A",
+            "id": {
+                "kind": "youtube#channel",
+                "channelId": "UCroOaW8bUO6HT3JEaLLARzA"
+            },
+            "snippet": {
+                "publishedAt": "2013-08-11T14:51:19Z",
+                "channelId": "UCroOaW8bUO6HT3JEaLLARzA",
+                "title": "SmartMantra",
+                "description": "My aim is to provide SMARTMANTRAs to people about Finance. Though I am more focused on finance (Share Market,Mutual ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://yt3.ggpht.com/ytc/AKedOLQuN0kef0UzhzZlrUINKBjQNYaTSf-5mQEQT2ti1g=s88-c-k-c0xffffffff-no-rj-mo"
+                    },
+                    "medium": {
+                        "url": "https://yt3.ggpht.com/ytc/AKedOLQuN0kef0UzhzZlrUINKBjQNYaTSf-5mQEQT2ti1g=s240-c-k-c0xffffffff-no-rj-mo"
+                    },
+                    "high": {
+                        "url": "https://yt3.ggpht.com/ytc/AKedOLQuN0kef0UzhzZlrUINKBjQNYaTSf-5mQEQT2ti1g=s800-c-k-c0xffffffff-no-rj-mo"
+                    }
+                },
+                "channelTitle": "SmartMantra",
+                "liveBroadcastContent": "upcoming",
+                "publishTime": "2013-08-11T14:51:19Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "Nc9AxFqsWYrARMRnXOKDGV8QwcY",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "51PtVBmGeGY"
+            },
+            "snippet": {
+                "publishedAt": "2022-01-05T15:56:44Z",
+                "channelId": "UCroOaW8bUO6HT3JEaLLARzA",
+                "title": "Stocks जो पिछले 5 सालो से लगातार 15% से ज्यादा Returns दे रहे है | SmartMantra",
+                "description": "Stocks जो पिछले 5 सालो से लगातार 15% से ज्यादा Returns दे रहे है | SmartMantra | smart mantra ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/51PtVBmGeGY/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/51PtVBmGeGY/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/51PtVBmGeGY/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "SmartMantra",
+                "liveBroadcastContent": "none",
+                "publishTime": "2022-01-05T15:56:44Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "XT4_QTSNusT-rrYB4lqY4NL5ijQ",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "Hmz53bgF6xI"
+            },
+            "snippet": {
+                "publishedAt": "2022-01-03T14:35:25Z",
+                "channelId": "UCroOaW8bUO6HT3JEaLLARzA",
+                "title": "Stocks Playlist for 2022 India Part 1 of 5",
+                "description": "Stocks Playlist for 2022 India Part 1 of 5 smartmantra smart mantra ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/Hmz53bgF6xI/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/Hmz53bgF6xI/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/Hmz53bgF6xI/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "SmartMantra",
+                "liveBroadcastContent": "none",
+                "publishTime": "2022-01-03T14:35:25Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "VoJWxhYYKXMZ7k-X3gf9chZlqx4",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "zZFUlgzk258"
+            },
+            "snippet": {
+                "publishedAt": "2022-01-03T15:28:36Z",
+                "channelId": "UCroOaW8bUO6HT3JEaLLARzA",
+                "title": "Best Penny Stock to buy now 2022 India | Stock under Rs 10 | SmartMantra",
+                "description": "Best Penny Stock to buy now 2022 India | Stock under Rs 10 | SmartMantra | smart mantra ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/zZFUlgzk258/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/zZFUlgzk258/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/zZFUlgzk258/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "SmartMantra",
+                "liveBroadcastContent": "none",
+                "publishTime": "2022-01-03T15:28:36Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "bIDFXH79aYHaE7Hw-AQwD7WgwQ4",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "NPCa4U4fFkI"
+            },
+            "snippet": {
+                "publishedAt": "2021-10-24T08:10:10Z",
+                "channelId": "UCroOaW8bUO6HT3JEaLLARzA",
+                "title": "5 SHORT TERM STOCKS TO BUY NOW | SMARTMANTRA",
+                "description": "5 SHORT TERM STOCKS TO BUY NOW | SMARTMANTRA Following queries may be resolved in this video: short term stocks to ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/NPCa4U4fFkI/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/NPCa4U4fFkI/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/NPCa4U4fFkI/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "SmartMantra",
+                "liveBroadcastContent": "none",
+                "publishTime": "2021-10-24T08:10:10Z"
+            }
+        }
+    ]
+}
+
+export const data2 = {
+    "kind": "youtube#searchListResponse",
+    "etag": "VHHDADHoN3mCM1eTwYvxeq2o6YE",
+    "nextPageToken": "CAUQAA",
+    "regionCode": "IN",
+    "pageInfo": {
+        "totalResults": 1000000,
+        "resultsPerPage": 5
+    },
+    "items": [
+        {
+            "kind": "youtube#searchResult",
+            "etag": "c1tDPuyyxyBYReE2Pcg-0Dqy7js",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "PkZNo7MFNFg"
+            },
+            "snippet": {
+                "publishedAt": "2018-12-10T14:13:40Z",
+                "channelId": "UC8butISFwT-Wl7EV0hUK0BQ",
+                "title": "Learn JavaScript - Full Course for Beginners",
+                "description": "This complete 134-part JavaScript tutorial for beginners will teach you everything you need to know to get started with the ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/PkZNo7MFNFg/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/PkZNo7MFNFg/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/PkZNo7MFNFg/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "freeCodeCamp.org",
+                "liveBroadcastContent": "none",
+                "publishTime": "2018-12-10T14:13:40Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "e6OrTxPN9fUcdetziP6-NTPdBOA",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "hKB-YGF14SY"
+            },
+            "snippet": {
+                "publishedAt": "2020-05-16T17:57:42Z",
+                "channelId": "UCeVMnSShP_Iviwkknt83cww",
+                "title": "JavaScript Tutorial In Hindi",
+                "description": "00:00 – Video Introduction 01:10 – JavaScript Introduction 02:43 – Basics of JavaScript 04:55 – Coding First Line of JavaScript ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/hKB-YGF14SY/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/hKB-YGF14SY/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/hKB-YGF14SY/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "CodeWithHarry",
+                "liveBroadcastContent": "none",
+                "publishTime": "2020-05-16T17:57:42Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "r5qPH0UVrcl67Q2C5Xv7Q80nGOY",
+            "id": {
+                "kind": "youtube#video",
+                "videoId": "W6NZfCO5SIk"
+            },
+            "snippet": {
+                "publishedAt": "2018-04-24T02:37:33Z",
+                "channelId": "UCWv7vMbMWH4-V0ZXdmDpPBA",
+                "title": "JavaScript Tutorial for Beginners: Learn JavaScript in 1 Hour",
+                "description": "Watch this JavaScript tutorial for beginners to learn JavaScript basics in one hour. Want to master JavaScript? Get my complete ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/W6NZfCO5SIk/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/W6NZfCO5SIk/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/W6NZfCO5SIk/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "Programming with Mosh",
+                "liveBroadcastContent": "none",
+                "publishTime": "2018-04-24T02:37:33Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "MVJ08sfVwRt3UEAMlSg6oUxkchI",
+            "id": {
+                "kind": "youtube#playlist",
+                "playlistId": "PLu0W_9lII9ajyk081To1Cbt2eI5913SsL"
+            },
+            "snippet": {
+                "publishedAt": "2019-07-11T04:52:44Z",
+                "channelId": "UCeVMnSShP_Iviwkknt83cww",
+                "title": "JavaScript Tutorials In Hindi",
+                "description": "In this javascript tutorial we will start javascript from the beginning and move towards the advanced level. JavaScript is a client ...",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/cvvwkgp4HBg/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/cvvwkgp4HBg/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/cvvwkgp4HBg/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "CodeWithHarry",
+                "liveBroadcastContent": "none",
+                "publishTime": "2019-07-11T04:52:44Z"
+            }
+        },
+        {
+            "kind": "youtube#searchResult",
+            "etag": "A_I-4R2T5K4poZe55P9BAW9gmO0",
+            "id": {
+                "kind": "youtube#playlist",
+                "playlistId": "PLsyeobzWxl7qtP8Lo9TReqUMkiOp446cV"
+            },
+            "snippet": {
+                "publishedAt": "2018-10-03T16:53:24Z",
+                "channelId": "UC59K-uG2A5ogwIrHw4bmlEg",
+                "title": "JavaScript Tutorial for Beginners",
+                "description": "",
+                "thumbnails": {
+                    "default": {
+                        "url": "https://i.ytimg.com/vi/uDwSnnhl1Ng/default.jpg",
+                        "width": 120,
+                        "height": 90
+                    },
+                    "medium": {
+                        "url": "https://i.ytimg.com/vi/uDwSnnhl1Ng/mqdefault.jpg",
+                        "width": 320,
+                        "height": 180
+                    },
+                    "high": {
+                        "url": "https://i.ytimg.com/vi/uDwSnnhl1Ng/hqdefault.jpg",
+                        "width": 480,
+                        "height": 360
+                    }
+                },
+                "channelTitle": "Telusko",
+                "liveBroadcastContent": "none",
+                "publishTime": "2018-10-03T16:53:24Z"
+            }
+        }
+    ]
+}
