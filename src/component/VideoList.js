5efc15aaa5858088e06beda0de2ecf5b14c7fa57
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-01-17 07:28:52 +0530

    background color is changed

--- src/component/VideoList.js
+++ src/component/VideoList.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
+import KeywordBar from './KeywordBar'
 
 const Container = styled.div`
 flex:5;
-background-color: lightgray;
+background-color: #181818;
 height: 100vh;
+width: 100%;
 
 `
 
 export default function VideoList() {
     return (
         <Container>
+            <KeywordBar/>
             from video
         </Container>
     )
