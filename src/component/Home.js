ca77d628c6f3cd37fdf7e81fc6f56e52182131cf
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-01-21 08:25:04 +0530

    Footer is added

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Mobile } from '../reponsive'
+import Footer from './Footer'
 import SideBar from './SideBar'
 import VideoList from './VideoList'
 
 const Container = styled.div`
 display: flex;
+/* flex-direction: column; */
+/* align-items: center;
+justify-content: center; */
+${Mobile({
+    flexDirection:'column',
+    alignItems:'center',
+    justifyContent:'center',
+    
+})} 
 `
 
 
@@ -14,6 +25,7 @@ export default function Home() {
         <Container>
             <SideBar  />
             <VideoList  />
+            <Footer/>
         </Container>
     )
 }
