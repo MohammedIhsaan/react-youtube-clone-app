1e9e0676a1b2f6785dba1677c67abf07380f4383
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-01-21 06:59:28 +0530

    two more media quaries adde

--- src/reponsive.js
+++ src/reponsive.js
@@ -7,6 +7,13 @@ export const Medium =(props)=>{
     }
     `
 }
+export const Medium1 =(props)=>{
+    return css`
+    @media only screen and (max-width:750px){
+        ${props}
+    }
+    `
+}
 export const Tablet =(props)=>{
     return css`
     @media only screen and (max-width:550px){
@@ -14,9 +21,23 @@ export const Tablet =(props)=>{
     }
     `
 }
+export const Tablet1 =(props)=>{
+    return css`
+    @media only screen and (max-width:480px){
+        ${props}
+    }
+    `
+}
 export const Mobile =(props)=>{
     return css`
     @media only screen and (max-width:420px){
+        ${props}
+    }
+    `
+}
+export const SmallMobile =(props)=>{
+    return css`
+    @media only screen and (max-width:280px){
         ${props}
     }
     `
