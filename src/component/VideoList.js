fc07d0b0be764eb52e641ddd0ff856951b5a2ed0
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-01-21 07:02:36 +0530

    worked on css

--- src/component/VideoList.js
+++ src/component/VideoList.js
@@ -1,30 +1,67 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
+import { AppContext } from '../App'
+import { Medium, Medium1, Mobile, Tablet } from '../reponsive'
 import KeywordBar from './KeywordBar'
 
 const Container = styled.div`
-flex:5;
+overflow-y: scroll;
+flex:18;
 background-color: #181818;
 height: 100vh;
-width: 94%;
+width:${props=>props.type==='halfSideBar'? '91%': '93%'};
+${Medium({
+    width: props=>props.type==='halfSideBar'? '86%': '90%'
+
+})}
 `
 const Wrap = styled.div`
 display: flex;
 align-items: center;
 justify-content: space-between;
 flex-wrap: wrap;
-width: 93%;
+width:${props=>props.type==='halfSideBar'? '95%': '95%'};
+padding-top: 30px;
+padding-left: 30px;
+
+
+
 `
 const Wrapper = styled.div`
-padding: 25px 0px 0px 30px;
-width: 20%;
+/* padding-bottom: 20px; */
+/* margin: 5px; */
+margin-bottom: 10px;
+border: 1px solid white;
+width: 22%;
+${Medium({
+    width:'30%',
+})}
+${Medium1({
+    width:'40%',
+})}
+${Tablet({
+    width:'90%',
+})}
+/* ${Mobile({
+    width:'90%',
+})} */
+
 `
 const Thumbnail = styled.div`
 cursor: pointer;
+/* width: 10%; */
+/* background-image: url('https://i.ytimg.com/an_webp/l9qr3EuLE_8/mqdefault_6s.webp?du=3000&sqp=CI3zmI8G&rs=AOn4CLDPeoncPj7Amo-oZ7x1gwY1Y225SA'); */
+/* object-fit: cover; */
 `
-const Image = styled.img`
-width: 320px;
-height: 200px;
+
+const Image = styled.div`
+border: 1px solid blue;
+/* width:${props=>props.type==='halfSideBar'? '230px': '280px'};
+height:${props=>props.type==='halfSideBar'? '140px': '150px'}; */
+
+width: 100%;
+height: 150px;
+/* height: 200px; */
 `
 const Span = styled.span`
 
@@ -39,15 +76,17 @@ const ChannelName = styled.div``
 const Views = styled.div``
 
 export default function VideoList() {
+    const {value}=useContext(AppContext)
+    console.log('value ......',value)
     return (
-        <Container>
-            <KeywordBar/>
-            <Wrap>
+        <Container type={value}>
+            <KeywordBar />
+            <Wrap type={value}>
                 {[1,2,3,4,5,1,1,1,1,11,1,1,1,11,1,11,1,11,1].map((e,i)=>{
                    return(
                         <Wrapper key={i}>
                    <Thumbnail>
-                       <Image src='https://i.ytimg.com/an_webp/l9qr3EuLE_8/mqdefault_6s.webp?du=3000&sqp=CI3zmI8G&rs=AOn4CLDPeoncPj7Amo-oZ7x1gwY1Y225SA'/>
+                       <Image/>
                        <Span>5min</Span>
                    </Thumbnail>
                    <Details>
