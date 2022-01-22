6ba945c52cc295d21509ef4f3f55ec0e9a1a0204
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-01-22 07:12:45 +0530

    worked on layout

--- src/component/VideoList.js
+++ src/component/VideoList.js
@@ -3,8 +3,11 @@ import styled from 'styled-components'
 import { AppContext } from '../App'
 import { Medium, Medium1, Mobile, Tablet } from '../reponsive'
 import KeywordBar from './KeywordBar'
+import myImg from '../images/ihsaan.jpeg'
+
 
 const Container = styled.div`
+color: white;
 overflow-y: scroll;
 flex:18;
 background-color: #181818;
@@ -32,21 +35,19 @@ padding-left: 30px;
 ${Mobile({
     paddingLeft: '0px',
     width: '100%',
-    height: '69vh',
+    height: '100vh',
 })}
 
 `
 const Wrapper = styled.div`
-/* padding-bottom: 20px; */
-/* margin: 5px; */
 margin-bottom: 10px;
 border: 1px solid white;
-width: 22%;
+width: 24%;
 ${Medium({
-    width:'30%',
+    width:'32%',
 })}
 ${Medium1({
-    width:'40%',
+    width:'48%',
 })}
 ${Tablet({
     width:'90%',
@@ -58,52 +59,92 @@ ${Mobile({
 `
 const Thumbnail = styled.div`
 cursor: pointer;
-/* width: 10%; */
-/* background-image: url('https://i.ytimg.com/an_webp/l9qr3EuLE_8/mqdefault_6s.webp?du=3000&sqp=CI3zmI8G&rs=AOn4CLDPeoncPj7Amo-oZ7x1gwY1Y225SA'); */
-/* object-fit: cover; */
+position: relative;
+border: 1px solid blue;
 `
 
-const Image = styled.div`
-border: 1px solid blue;
-/* width:${props=>props.type==='halfSideBar'? '230px': '280px'};
-height:${props=>props.type==='halfSideBar'? '140px': '150px'}; */
-
+const Image = styled.img`
 width: 100%;
 height: 150px;
-/* height: 200px; */
+z-index: 1;
+
+${Mobile({
+    width: '100%',
+height: '200px',
+
+})}
 `
 const Span = styled.span`
-
+position: absolute;
+bottom: 20px;
+right: 20px;
+z-index: 3;
+background-color: black;
+width: 50px;
 `
 const Details = styled.div`
 cursor: pointer;
+display: flex;
 `
-const ChannelIcon = styled.div``
-const VideoDtails = styled.div``
+const ChannelIcon = styled.div`
+flex: 1;
+display: flex;
+padding: 10px;
+`
+const ChannelImage = styled.img`
+width: 35px;
+height: 35px;
+border-radius: 50%;
+`
+const VideoDtails = styled.div`
+display: flex;
+flex: 8;
+flex-direction: column;
+padding-top: 8px;
+`
+const TitleDiv = styled.div`
+font-size: 16px;
+padding-bottom: 3px;
+height: 38px;
+width: 100%;
+overflow: hidden;
+`
 const Title = styled.div``
-const ChannelName = styled.div``
+const ChannelName = styled.div`
+padding-right: 5px;
+`
+const ChannelDiv = styled.div`
+color: lightgrey;
+display: flex;
+font-weight: 100;
+`
 const Views = styled.div``
 
 export default function VideoList() {
-    const {value}=useContext(AppContext)
-    console.log('value ......',value)
+    const {value,data2}=useContext(AppContext)
+    console.log('data2 ......',data2.items)
     return (
         <Container type={value}>
             <KeywordBar />
             <Wrap type={value}>
-                {[1,2,3,4,5,1,1,1,1,11,1,1,1,11,1,11,1,11,1].map((e,i)=>{
+                {data2.items.map((e,i)=>{
                    return(
                         <Wrapper key={i}>
                    <Thumbnail>
-                       <Image/>
+                       <Image src={e.snippet.thumbnails.high.url}>
+                       </Image>
                        <Span>5min</Span>
                    </Thumbnail>
                    <Details>
-                       <ChannelIcon>Icon</ChannelIcon>
+                       <ChannelIcon><ChannelImage src={myImg}/></ChannelIcon>
                        <VideoDtails>
-                           <Title>youtube clone app tutorial</Title>
-                           <ChannelName>Ihsaan.Dev</ChannelName>
-                           <Views>26k views : 18 hours ago</Views>
+                           <TitleDiv>
+                           <Title>{e.snippet.title}</Title>
+                           </TitleDiv>
+                           <ChannelDiv>
+                           <ChannelName>{e.snippet.channelTitle}</ChannelName>
+                           <Views> • 26k views • 18 hours ago</Views>
+                           </ChannelDiv>
    
                        </VideoDtails>
                    </Details>
