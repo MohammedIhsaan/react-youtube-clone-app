f4c037dcdf916bfeee13e09c4489eb17c0a25f3e
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-08-08 22:19:08 +0530

    test2

--- src/component/NewComponentForTest.js
+++ src/component/NewComponentForTest.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function NewComponentForTest() {
   return (
-    <div>NewComponentForTest</div>
+    <div>NewComponentForTest
+        <span>from test branch</span>
+    </div>
   )
 }
--- src/component/TestComponent.js
+++ src/component/TestComponent.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function TestComponent() {
   return (
-    <div>TestComponent</div>
+    <div>TestComponent
+      <span>from test branch</span>
+    </div>
   )
 }
