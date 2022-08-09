c079699cf9baa7a8d9b95966e0b9e545339e635b
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-08-09 11:47:08 +0530

    in featured branch

--- src/component/TeamMemberComponent.js
+++ src/component/TeamMemberComponent.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function TeamMemberComponent() {
   return (
-    <div>TeamMemberComponent</div>
+    <div>TeamMemberComponent
+        <span>hello</span>
+    </div>
   )
 }
