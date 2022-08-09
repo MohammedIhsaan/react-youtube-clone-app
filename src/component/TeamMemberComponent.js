729a573a77d65755381b44d61a4d072e2159b5fd
MohammedIhsaan <mohammedihsaanulhaque@gmail.com>
2022-08-09 12:23:40 +0530

    new updates

--- src/component/TeamMemberComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function TeamMemberComponent() {
-  return (
-    <div>TeamMemberComponent
-        <span>hello</span>
-    </div>
-  )
-}
--- src/component/loginComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function loginComponent() {
-  return (
-    <div>loginComponent</div>
-  )
-}
